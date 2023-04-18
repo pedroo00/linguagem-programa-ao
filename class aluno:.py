69b8eeef1be7c97ee07973c57b6ee78300dfbd52
pedroo00 <130671216+pedroo00@users.noreply.github.com>
2023-04-18 08:47:58 -0300

    Add files via upload

---
+++ class aluno:.py
@@ -0,0 +1,16 @@
+class aluno:
+    def int (self,matricula,nome,nota1,nota2, nota_trabalho)
+        self.matricula = matricula
+        self.nome = nome
+        self.nota1 = nota1
+        self.nota2 = nota2
+        self.nota_trabalho = nota_trabalho
+
+def resultado (self);
+    media = (self.nota1 = self.nota2 = self.nota_trabalho) / 3
+    if media <4.0:
+        return "reprovado"
+    elif media <= 7.0;
+        return "aprovado"
+    else:
+        return f"final: (10.0 - media:.2f)"
